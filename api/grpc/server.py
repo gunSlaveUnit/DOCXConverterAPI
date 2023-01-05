0259da143bff1367b3efdaf49521a2359c203644
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-01-05 13:36:36 +0300

    Server can run now

--- api/grpc/server.py
+++ api/grpc/server.py
@@ -1,7 +1,30 @@
-from api.grpc.merger_pb2_grpc import MergerServicer
+import logging
+from concurrent import futures
+
+import grpc
+
+import merger_pb2
+import merger_pb2_grpc
+from utilities.merger import clear_files_directory, combine_documents
 
 
-class GRPCDOCXMerger(MergerServicer):
-    def merge(self, request, context):
-        print(request)
-        print(context)
+class GRPCDOCXMerger(merger_pb2_grpc.MergerServicer):
+    async def Merge(self, request, context):
+        response = await combine_documents(request.docs)
+        clear_files_directory()
+        return merger_pb2.MergeResponse(doc=response)
+
+
+def serve():
+    port = '50051'
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    merger_pb2_grpc.add_MergerServicer_to_server(GRPCDOCXMerger(), server)
+    server.add_insecure_port('[::]:' + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
